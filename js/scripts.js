bd2fbbcabec1196945e5d89d1d1d9bc116161b01
Anurag251 <71079995+Anurag251@users.noreply.github.com>
2021-01-20 23:31:50 +0545

    finance
    finance update

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,6 @@
-window.onload = function () {
-  window.addEventListener('scroll', () => {
-    let nav = document.querySelector('nav, .logo');
-    nav.classList.toggle('sticky', window.scrollY > 100);
-  });
-
-  let phone = document.querySelector('.phone');
-  const menuBtn = document.querySelector('.menu-btn');
-  const close = document.querySelector('.close-btn');
-  let menuOpen = false;
-  close.addEventListener('click', () => {
-    phone.style.width = '0';
-    menuBtn.classList.remove('open');
-    menuOpen = false;
-  });
-  menuBtn.addEventListener('click', () => {
-    if (!menuOpen) {
-      menuBtn.classList.add('open');
-      phone.style.width = '300px';
-      menuOpen = true;
-    } else {
-      menuBtn.classList.remove('open');
-      phone.style.width = '0';
-      menuOpen = false;
-    }
-  });
+const navbar = document.querySelector('.navbars');
+window.onscroll = () => {
+  this.scrollY > 20
+    ? navbar.classList.add('sticky')
+    : navbar.classList.remove('sticky');
 };
-
-var x = 1;
-
-for (x; x < 6; x += 2) {
-  x = x * x;
-}
-
-console.log(x);
